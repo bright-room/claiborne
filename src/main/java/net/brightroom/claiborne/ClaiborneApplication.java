b503900f17f2d66ffb25bc1564812ec0a2f7f5d3
kukv <rio.by.kukv@gmail.com>
2025-03-31 22:51:47 +0900

    initial commit

---
+++ src/main/java/net/brightroom/claiborne/ClaiborneApplication.java
@@ -0,0 +1,13 @@
+package net.brightroom.claiborne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaiborneApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ClaiborneApplication.class, args);
+  }
+
+}
